b33b2d60175b12cd731a3c508cc07d82136f211a
CedricA <cedric.j.aoun@gmail.com>
2025-12-16 11:38:44 +0100

    exo 12: use array with forelse loop to display articles

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,8 +6,22 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    private $articles = [
+        [
+            'title' => 'L’IA soigne mieux',
+            'description' => "L’intelligence artificielle aide les médecins à diagnostiquer plus vite."
+        ],
+        [
+            'title' => 'Villes vertes',
+            'description' => 'Les métropoles deviennent plus écologiques et durables.'
+        ],
+        [
+            'title' => 'Télétravail',
+            'description' => 'Plus de liberté, mais aussi plus de solitude.'
+        ]
+    ];
     public function index()
     {
-        return view('welcome', ['name' => 'Cedric']);
+        return view('welcome', ['name' => 'Cedric', 'articles' => $this->articles]);
     }
 }
