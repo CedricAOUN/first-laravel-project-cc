3a9a749fd7b4d5d44d0d675babdc184bb81b1c4c
CedricA <cedric.j.aoun@gmail.com>
2025-12-16 10:44:08 +0100

    exo 11: add Article component with 3 instances in home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,11 @@
 @section('title', config('app.name', 'Laravel'))
 
 @section('content')
-    <h2>Bienvenue sur le site de {{ $name }} !</h2>
+<h2>Bienvenue sur le site de {{ $name }} !</h2>
+<h2>Liste des articles : </h2>
+<div>
+    <x-article title="L’IA soigne mieux" description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite." />
+    <x-article title="Villes vertes" description="Les métropoles deviennent plus écologiques et durables." />
+    <x-article title="Télétravail" description="Plus de liberté, mais aussi plus de solitude." />
+</div>
 @endsection
